bfde595ce17c6ef93317424f6ba0a619e8efb81f
rail <rail.shabayev@mail.ru>
2024-04-09 05:03:06 +0400

    feat: add fixed services

--- chat-service/src/main/java/org/rail/service/ChatService.java
+++ chat-service/src/main/java/org/rail/service/ChatService.java
@@ -1,7 +1,32 @@
 package org.rail.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.rail.model.Request;
+import org.rail.model.Response;
+import org.rail.repository.RequestRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
 
 @Service
+@RequiredArgsConstructor
 public class ChatService {
+    private final RequestRepository requestRepository;
+    private final RestClient restClient;
+    private final ObjectMapper objectMapper;
+
+    public Response makeRequest(Request request) throws JsonProcessingException {
+        //Repository saving
+        String body = objectMapper.writeValueAsString(request);
+        System.out.println(body);
+        ResponseEntity<Response> response = restClient
+                .post()
+                .body(body)
+                .retrieve()
+                .toEntity(Response.class);
+        System.out.println(response);
+        return response.getBody();
+    }
 }

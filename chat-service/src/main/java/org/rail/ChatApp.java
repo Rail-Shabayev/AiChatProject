7d32c77ec89672dcf3b61e3f83cfcd0d83718c55
rail <rail.shabayev@mail.ru>
2024-04-10 04:47:30 +0400

    feat: add command line runner

--- chat-service/src/main/java/org/rail/ChatApp.java
+++ chat-service/src/main/java/org/rail/ChatApp.java
@@ -2,13 +2,31 @@ package org.rail;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import org.rail.model.Request;
+import org.rail.model.Response;
+import org.rail.repository.RequestRepository;
+import org.rail.repository.ResponseRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(description = "third project"))
 public class ChatApp {
     public static void main(String[] args) {
         SpringApplication.run(ChatApp.class);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(RequestRepository requestRepository, ResponseRepository responseRepository) {
+        return args -> {
+            Response response1 = new Response(1L, LocalDateTime.now(), "the", true);
+            responseRepository.save(response1);
+            requestRepository.save(new Request(UUID.randomUUID(), "llama2", "what is", true, false, response1));
+        };
     }
 }
